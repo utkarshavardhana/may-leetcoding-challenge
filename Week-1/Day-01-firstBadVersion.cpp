8cff0585e6d000d7cfcf83956f82120bbbf0ede5
UTKARSHA VARDHANA <utkarshavardhana85@gmail.com>
2020-05-02 09:48:51 +0530

    Add files via upload

---
+++ Week-1/Day-01-firstBadVersion.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo = 1, hi = n, mid = 0;
+        while(lo < hi) {
+            mid = lo + (hi-lo)/2;
+            if(isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+};
