1bec3b21c0920845fc8af2f9ac48aad819a4dff7
UTKARSHA VARDHANA <utkarshavardhana85@gmail.com>
2020-05-16 08:16:59 +0530

    Add files via upload

---
+++ Week-3/Day-15-maxSubarraySumCircular.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+        for (int a : A) {
+            curMax = max(curMax + a, a);
+            maxSum = max(maxSum, curMax);
+            curMin = min(curMin + a, a);
+            minSum = min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
+    }
+};
