1f09df2fcf72f4f75d0b6123e186d980ee3c23a3
UTKARSHA VARDHANA <utkarshavardhana85@gmail.com>
2020-05-24 10:39:16 +0530

    Add files via upload

---
+++ Week-4/Day-23-intervalIntersection.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> res;
+        for (int i = 0, j = 0; i < A.size() && j < B.size(); A[i][1] < B[j][1] ? ++i : ++j) {
+            int start = max(A[i][0], B[j][0]);
+            int end = min(A[i][1], B[j][1]);
+            if (start <= end)   res.push_back({start, end});
+        }
+        return res;
+    }
+};
